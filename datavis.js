ecbb4d8c69dbd94b2999d36b6e9f75a2d0f52055
NishatFiroj <nishat.firoj@gmail.com>
2018-10-20 16:42:15 -0400

    adjusted data vis

--- datavis.js
+++ datavis.js
@@ -1,38 +1,45 @@
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ["Java", "C", "MATLAB", "HTML/CSS", "JavaScript"],
-        datasets: [{
-            data: [19, 10, 5, 19, 10],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ]
-        }]
+  type: 'doughnut',
+  data: {
+    labels: ["Java", "C", "MATLAB", "HTML/CSS", "JavaScript"],
+    datasets: [{
+      data: [19, 10, 5, 19, 10],
+      backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(255, 159, 64, 0.2)'
+      ]
+    }]
+  },
+  options: {
+    legend: {
+      display: true,
+      position: 'bottom'
     },
-    options: {
-        scales: {
-            yAxes: [{
-                gridLines: {
-                  color: "rgba(0, 0, 0, 0)",
-                  drawBorder: false,
-                  display: false,
-                  ticks: {
-                    fontColor: "rgba(0, 0, 0, 0)",
-                  }
-                }
-              }],
-            xAxes: [{
-                gridLines: {
-                  color: "rgba(0, 0, 0, 0)",
-                  drawBorder: false,
-                  display: false,
-                }
-            }]
+    scales: {
+      yAxes: [{
+        gridLines: {
+          color: "rgba(0, 0, 0, 0)",
+          drawBorder: false,
+          display: false,
+          ticks: {
+            display: false
+          }
         }
+      }],
+      xAxes: [{
+        gridLines: {
+          color: "rgba(0, 0, 0, 0)",
+          drawBorder: false,
+          display: false,
+          ticks: {
+            display: false
+          }
+        }
+      }]
     }
+  }
 });
